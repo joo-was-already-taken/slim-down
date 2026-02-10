c995002a0e5127a9e9981148a36f64f96f5f80a2
joo-was-already-taken <trackpointus@protonmail.com>
2026-02-11 00:22:38 +0100

    add more integration tests

--- tests/integration.zig
+++ tests/integration.zig
@@ -52,6 +52,104 @@ test "ignore patterns" {
     try expectError(error.FileNotFound, tmp.dir.statFile("garbage.log"));
 }
 
+test "relative patterns are anchored to cwd" {
+    const allocator = std.testing.allocator;
+    var tmp = std.testing.tmpDir(.{});
+    defer tmp.cleanup();
+
+    try tmp.dir.writeFile(.{ .sub_path = "root.txt", .data = "" });
+    try tmp.dir.makeDir("sub");
+    try tmp.dir.writeFile(.{ .sub_path = "sub/nested.txt", .data = "" });
+    try tmp.dir.writeFile(.{ .sub_path = "sub/root.txt", .data = "" });
+
+    try runSlimDown(allocator, tmp.dir, &.{ "-r", "root.txt", "-r", "nested.txt", "--no-config" });
+
+    try expectError(error.FileNotFound, tmp.dir.statFile("root.txt"));
+
+    _ = try tmp.dir.statFile("sub/root.txt");
+    _ = try tmp.dir.statFile("sub/nested.txt");
+}
+
+test "ignore takes precedence over remove" {
+    const allocator = std.testing.allocator;
+    var tmp = std.testing.tmpDir(.{});
+    defer tmp.cleanup();
+
+    try tmp.dir.writeFile(.{ .sub_path = "delete_me.txt", .data = "" });
+    try tmp.dir.writeFile(.{ .sub_path = "keep_me.txt", .data = "" });
+    try tmp.dir.makeDir("subdir");
+    try tmp.dir.writeFile(.{ .sub_path = "subdir/delete_me.txt", .data = "" });
+    try tmp.dir.writeFile(.{ .sub_path = "subdir/keep_me.txt", .data = "" });
+
+    try runSlimDown(allocator, tmp.dir, &.{
+        "-r", "**/*.txt",
+        "-i", "**/*keep_me.txt",
+        "--no-config",
+    });
+
+    try expectError(error.FileNotFound, tmp.dir.statFile("delete_me.txt"));
+    try expectError(error.FileNotFound, tmp.dir.statFile("subdir/delete_me.txt"));
+
+    _ = try tmp.dir.statFile("keep_me.txt");
+    _ = try tmp.dir.statFile("subdir/keep_me.txt");
+}
+
+test "double star wildcard resolution" {
+    const allocator = std.testing.allocator;
+    var tmp = std.testing.tmpDir(.{});
+    defer tmp.cleanup();
+
+    // .
+    // ├── deep
+    // │   ├── a
+    // │   │   └── target.log
+    // │   └── target.log
+    // ├── shallow
+    // │   └── target.log
+    // ├── target.log
+    // └── other.log
+
+    try tmp.dir.makePath("deep/a");
+    try tmp.dir.makeDir("shallow");
+    
+    try tmp.dir.writeFile(.{ .sub_path = "deep/a/target.log", .data = "" });
+    try tmp.dir.writeFile(.{ .sub_path = "deep/target.log", .data = "" });
+    try tmp.dir.writeFile(.{ .sub_path = "shallow/target.log", .data = "" });
+    try tmp.dir.writeFile(.{ .sub_path = "target.log", .data = "" });
+    try tmp.dir.writeFile(.{ .sub_path = "other.log", .data = "" });
+
+    try runSlimDown(allocator, tmp.dir, &.{ "-r", "**/target.log", "--no-config" });
+
+    try expectError(error.FileNotFound, tmp.dir.statFile("deep/a/target.log"));
+    try expectError(error.FileNotFound, tmp.dir.statFile("deep/target.log"));
+    try expectError(error.FileNotFound, tmp.dir.statFile("shallow/target.log"));
+    
+    try expectError(error.FileNotFound, tmp.dir.statFile("target.log"));
+
+    _ = try tmp.dir.statFile("other.log");
+}
+
+test "specific double star usage" {
+    const allocator = std.testing.allocator;
+    var tmp = std.testing.tmpDir(.{});
+    defer tmp.cleanup();
+
+    try tmp.dir.makePath("a/b/c");
+    try tmp.dir.makePath("a/c");
+    try tmp.dir.makePath("b/c");
+    
+    try tmp.dir.writeFile(.{ .sub_path = "a/b/c/file", .data = "" });
+    try tmp.dir.writeFile(.{ .sub_path = "a/c/file", .data = "" });
+    try tmp.dir.writeFile(.{ .sub_path = "b/c/file", .data = "" });
+
+    try runSlimDown(allocator, tmp.dir, &.{ "-r", "a/**/c/file", "--no-config" });
+
+    try expectError(error.FileNotFound, tmp.dir.statFile("a/b/c/file"));
+    try expectError(error.FileNotFound, tmp.dir.statFile("a/c/file"));
+    
+    _ = try tmp.dir.statFile("b/c/file");
+}
+
 test "nested structure with config" {
     const allocator = std.testing.allocator;
     var tmp = std.testing.tmpDir(.{});
@@ -84,6 +182,7 @@ test "nested structure with config" {
 
     _ = try tmp.dir.statFile("root.js");
 }
+
 fn runSlimDown(allocator: std.mem.Allocator, dir: std.fs.Dir, args: []const []const u8) !void {
     return runSlimDownWithEnv(allocator, dir, args, null);
 }
